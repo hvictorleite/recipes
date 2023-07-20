4079b6ea525ce8b59be0e69847169197939e58ec
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-07-20 15:24:00 -0300

    Created recipe model. Displaying a list of recipes using ngFor.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply recipe example.',
+      'https://cdn-icons-png.flaticon.com/512/683/683488.png'),
+    new Recipe('A Test Recipe', 'This is a simply recipe example.',
+      'https://cdn-icons-png.flaticon.com/512/683/683488.png')
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
