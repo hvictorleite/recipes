f96a12791f855294a8ec97b3aa0d2bcb81c9837c
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-07-28 17:23:56 -0300

    Created tab navigation  using ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  collapsed: boolean = true;
+  @Output() tabSelected = new EventEmitter<string>();
+
+  onSelectTab(tabName: string): void {
+    this.tabSelected.emit(tabName);
+  }
 }
