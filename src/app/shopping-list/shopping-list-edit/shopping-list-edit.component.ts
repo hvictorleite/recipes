9aa287569971b5b81749e590f3b11ae17722dbe7
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-01 17:17:40 -0300

    Adding a new ingredient using @ViewChild

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
+  @ViewChild('nameInput', {static: false}) nameInput: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInput: ElementRef;
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddIngredient(): void {
+    const name: string = this.nameInput.nativeElement.value;
+    const amount: number = Number(this.amountInput.nativeElement.value);
+
+    if (name !== '' && amount <= 0) {
+      console.error('ERROR: Ingredient addition failed. Invalid input values.');
+      return;
+    }
+
+    this.ingredientAdded.emit(
+      new Ingredient(this.nameInput.nativeElement.value, Number(this.amountInput.nativeElement.value))
+    );
+  }
 }
