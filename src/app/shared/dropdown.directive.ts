4d588e64021c43589ee98d0c29002fe0eb2701d4
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-03 10:40:31 -0300

    Using dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.show') isOpen: boolean = false;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('click') toggleShow(): void {
+    const element = this.elRef.nativeElement;
+
+    this.isOpen = element.contains(event.target) ? !this.isOpen : false;
+    const part = element.querySelector('.dropdown-menu');
+
+    if (this.isOpen)
+      this.renderer.addClass(part, 'show');
+    else this.renderer.removeClass(part, 'show');
+  }
+
+  @HostListener('focusout') closeFromAnywhere(): void {
+    const element = this.elRef.nativeElement;
+    const part = element.querySelector('.dropdown-menu');
+
+    if (this.isOpen) {
+      this.renderer.removeClass(part, 'show');
+      this.isOpen = false;
+    }
+  }
+}
