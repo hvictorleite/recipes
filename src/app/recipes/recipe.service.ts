89736c05b1b7daeb6a978de9935ee92cfdf615fb
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-11 16:38:18 -0300

    Using a service for cross component comunication to select a recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a simply test',
       'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_640.png'),
