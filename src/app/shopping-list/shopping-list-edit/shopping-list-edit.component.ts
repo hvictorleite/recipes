1c38609e9ebf420636cc9c332b01f2830d4f4f56
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-14 12:56:25 -0300

    Fixed addIngredient method

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
