016635a45ca2650b38d7374271068e2357723ff1
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-15 08:52:38 -0300

    Adding ingredients to recipe and displaying in recipe details.

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = description;
-    this.imagePath = imagePath;
+    this.imagePath = imagePath !== '' ? imagePath : 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_640.png';
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,25 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simply test',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_640.png'),
-      new Recipe('A Another Test Recipe', 'This is a another simply test',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_640.png')
+    new Recipe('Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://as2.ftcdn.net/v2/jpg/01/38/80/65/1000_F_138806543_2qnuaBOHp8DrIQGaRFjyft2JwzX885K0.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say??',
+      'https://cdn-cmjom.nitrocdn.com/FpMsHpAgoVrRMnuAdmBhGkyiizdsWlSU/assets/images/optimized/rev-dd2c928/wp-content/uploads/2015/07/king-burger-541x633.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ])
   ];
 
   getRecipes(): Recipe[] {
