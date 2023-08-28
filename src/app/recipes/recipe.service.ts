305cd52375af5df388acff4887b076a95bbfdda8
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-28 17:06:44 -0300

    Added 'id' to recipe model and getRecipe method to service.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,14 +9,16 @@ export class RecipeService {
   ingredientsAdded = new EventEmitter<Ingredient[]>();
 
   private recipes: Recipe[] = [
-    new Recipe('Tasty Schnitzel',
+    new Recipe(1,
+      'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
       'https://as2.ftcdn.net/v2/jpg/01/38/80/65/1000_F_138806543_2qnuaBOHp8DrIQGaRFjyft2JwzX885K0.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
-    new Recipe('Big Fat Burger',
+    new Recipe(2,
+      'Big Fat Burger',
       'What else you need to say??',
       'https://cdn-cmjom.nitrocdn.com/FpMsHpAgoVrRMnuAdmBhGkyiizdsWlSU/assets/images/optimized/rev-dd2c928/wp-content/uploads/2015/07/king-burger-541x633.png',
       [
@@ -29,6 +31,10 @@ export class RecipeService {
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number): Recipe {
+    return this.getRecipes()[id];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
