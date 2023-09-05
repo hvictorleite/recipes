e574be5789e57af7e025b25ec7b7a75d09a4e54c
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-05 09:06:17 -0300

    Template-Driven form with validation created to ShoppingListEdit component.

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -8,19 +9,13 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
-  @ViewChild('nameInput', {static: false}) nameInput: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInput: ElementRef;
+  @ViewChild('SlEditForm', {static: false}) SlEditForm: NgForm;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   onAddIngredient(): void {
-    const name: string = this.nameInput.nativeElement.value;
-    const amount: number = Number(this.amountInput.nativeElement.value);
-
-    if (name !== '' && amount <= 0) {
-      console.error('ERROR: Ingredient addition failed. Invalid input values.');
-      return;
-    }
+    const name: string = this.SlEditForm.value.name;
+    const amount: number = +this.SlEditForm.value.amount;
 
     this.shoppingListService.addIngredient(
       new Ingredient(name, amount)
