496a118338a8cd7af6478c3285c940fefac2d3e2
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-13 15:45:49 -0300

    Storing and Fetching recipes. Transforming response data.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,26 +10,33 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   ingredientsAdded = new Subject<Ingredient[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(1,
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://as2.ftcdn.net/v2/jpg/01/38/80/65/1000_F_138806543_2qnuaBOHp8DrIQGaRFjyft2JwzX885K0.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe(2,
-      'Big Fat Burger',
-      'What else you need to say??',
-      'https://cdn-cmjom.nitrocdn.com/FpMsHpAgoVrRMnuAdmBhGkyiizdsWlSU/assets/images/optimized/rev-dd2c928/wp-content/uploads/2015/07/king-burger-541x633.png',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1),
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(1,
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://as2.ftcdn.net/v2/jpg/01/38/80/65/1000_F_138806543_2qnuaBOHp8DrIQGaRFjyft2JwzX885K0.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe(2,
+  //     'Big Fat Burger',
+  //     'What else you need to say??',
+  //     'https://cdn-cmjom.nitrocdn.com/FpMsHpAgoVrRMnuAdmBhGkyiizdsWlSU/assets/images/optimized/rev-dd2c928/wp-content/uploads/2015/07/king-burger-541x633.png',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1),
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,33 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http
+      .put(
+        'https://recipes-f9b0f-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      ).subscribe({
+        next: response => console.log(response)
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>('https://recipes-f9b0f-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe =>
+          { return { ...recipe, ingredients: recipe.ingredients ?? [] } })
+      }))
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
 }
