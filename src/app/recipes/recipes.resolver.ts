caf70d270c3facefed16cd429444c06905afb4e0
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-13 19:00:40 -0300

    Fixing a bug with the resolver

--- src/app/recipes/recipes.resolver.ts
+++ src/app/recipes/recipes.resolver.ts
@@ -3,9 +3,15 @@ import { Recipe } from "./recipe.model";
 import { inject } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Observable } from "rxjs";
+import { RecipeService } from "./recipe.service";
 
 export const RecipesResolver: ResolveFn<Recipe[]> = (
   route: ActivatedRouteSnapshot, state: RouterStateSnapshot
   ): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[]  => {
-    return inject(DataStorageService).fetchRecipes();
+    const recipes = inject(RecipeService).getRecipes();
+
+    if (recipes.length === 0)
+      return inject(DataStorageService).fetchRecipes();
+    else
+      return recipes;
   }
