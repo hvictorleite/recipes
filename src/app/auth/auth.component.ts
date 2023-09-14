4798af48085c02ea3db57ac108ece412c69de070
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-14 17:21:07 -0300

    Sending the signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -8,12 +9,30 @@ import { NgForm } from "@angular/forms";
 export class AuthComponent {
   isLoginMode = true;
 
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    if (!form.valid) return;
+
+    const email: string = form.value.email;
+    const password: string = form.value.password;
+
+    if (this.isLoginMode) {
+      // ...
+    }
+    else {
+      this.authService.signup(email, password)
+        .subscribe({
+          next: resData => {
+            console.log(resData);
+          },
+          error: err => console.log(err)
+        });
+    }
     form.reset();
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 interface AuthResponseData {
   idToken: string,
@@ -7,15 +8,16 @@ interface AuthResponseData {
   refreshToken: string,
   expiresIn: string,
   localId: string,
+  registered: boolean
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  signup(email: string, password: string) {
+  signup(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJuWWR-c98eXUFF08UGpAF_DP--cy0ZsA',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCJuWWR-c98eXUFF08UGpAF_DP--cy0ZsA',
       {
         email: email,
         password: password,
