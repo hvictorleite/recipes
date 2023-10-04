7cf40cc98a5e74e546c9abd81d7eb24ae8878e3a
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-10-04 11:56:26 -0300

    Dynamic component with message and capable of closing.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
 
 import { AlertComponent } from "../shared/alert/alert.component";
@@ -11,11 +11,13 @@ import { PlaceholderDirective } from "../shared/placeholder/placeholder.directiv
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy{
   isLoginMode = true;
   isLoading = false;
   error: string = null;
   @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -61,8 +63,17 @@ export class AuthComponent {
   showErrorAlert(message: string) {
     const hostViewContainerRef = this.alertHost.viewContainerRef;
     hostViewContainerRef.clear();
-    hostViewContainerRef.createComponent(AlertComponent);
+    const componentRef = hostViewContainerRef.createComponent(AlertComponent);
+
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 
-
+  ngOnDestroy(): void {
+    if (this.closeSub)
+      this.closeSub.unsubscribe();
+  }
 }
